77cfe57e05177f8de2c736daefa9ecd27fe4f0ff
Thiago Alves <thiagoalves@itatiaia.com.br>
2018-09-22 14:50:05 +0000

    Alterações

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
   resources :professions
   resources :ubs
   resources :pacients
-  
+
   resources :attendances
   resources :prm_causes
   resources :pharmacotherapies
   resources :prms
   resources :diseases
   resources :type_sexes
+  
+  get "/type_sexes" => redirect('https://gerfar-2-henriquebatista.c9users.io/type_sexes');
+  get "/pacients" => redirect('https://gerfar-2-henriquebatista.c9users.io/pacients');
+  get "/prm_causes" => redirect('https://gerfar-2-henriquebatista.c9users.io/prm_causes');
+  get "/prms" => redirect('https://gerfar-2-henriquebatista.c9users.io/prms');
+  get "/attendances" => redirect('https://gerfar-2-henriquebatista.c9users.io/attendances');
   
   resources :treatments
   resources :sfcs
