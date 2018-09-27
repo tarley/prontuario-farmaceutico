7b1138c0f55eda1a2ccc1dd44481c99811be4c19
Pablo Zambaldi <pablozamba@hotmail.com>
2018-09-27 20:01:53 +0000

    Validações campos paciente

--- app/models/pacient.rb
+++ app/models/pacient.rb
@@ -9,12 +9,21 @@ class Pacient < ActiveRecord::Base
     
     
     validates_presence_of :name, message: 'não pode ser deixado em branco'
-    validates_presence_of :phone, message: 'não pode ser deixado em branco'
-    validates_presence_of :cpf, message: 'não pode ser deixado em branco'
     
     validates_length_of :name, minimum: 3, message: 'deve ter pelo menos 3 caracteres'
-    validates_length_of :phone, maximum: 11, message: 'deve ter no máximo 11 caracteres'
     
-    validates_uniqueness_of :cpf, message: 'Este CPF já está cadastrado!'
+    validates :phone, presence: {message: 'não pode ser deixado em branco'},
+                        numericality: {message: 'deve conter somente número'},
+                        length: {minimum: 10, message: 'deve conter no mínimo 10 números'}
+    
+    validates :years_study,     numericality: {:greater_than_or_equal_to  => 0, message: 'deve ser maior ou igual a zero'}
+    
+
+    validates :cpf, presence: {message: 'não pode ser deixado em branco'},
+                        numericality: {message: 'deve conter somente número!'},
+                        length: {is: 11, message: 'deve conter 11 números!'}    
+                                
+    validates_uniqueness_of :cpf, message: 'Este CPF já está cadastrado!'                    
+    
     
 end
--- app/models/type_sex.rb
+++ app/models/type_sex.rb
@@ -1,5 +1,5 @@
 class TypeSex < ActiveRecord::Base
     
     validates_uniqueness_of :description, message: 'Esse Sexo já está cadastrado!'
-    
+
 end
