ac1490d03e8208936d24ba775df1fa4ced381e38
Thiago Alves <thiagoalves@itatiaia.com.br>
2018-10-01 11:36:31 +0000

    Campos Obrigatórios

--- app/models/pharmacotherapy.rb
+++ app/models/pharmacotherapy.rb
@@ -1,7 +1,15 @@
 class Pharmacotherapy < ActiveRecord::Base
   belongs_to :treatment
   
-  validates_presence_of :conduct, message: 'não pode ser deixado em branco'
+  validates_presence_of :medicine, message: 'não pode ser deixado em branco'
+  validates_presence_of :dosageSchedule, message: 'não pode ser deixado em branco'
+  validates_presence_of :safety, message: 'não pode ser deixado em branco'
+  validates_presence_of :effectiveness, message: 'não pode ser deixado em branco'
+  validates_presence_of :timeUse, message: 'não pode ser deixado em branco'
+  validates_presence_of :carePlan, message: 'não pode ser deixado em branco'
+  validates_presence_of :descriptionDisease, message: 'não pode ser deixado em branco'
+  
+  
   
   
 end
