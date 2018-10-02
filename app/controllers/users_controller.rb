7efdfce68afc40d666a3e2e0a86ce9c3fe507aa6
Tarley <tarley.lana@gmail.com>
2018-10-02 01:07:37 +0000

    Alterações locais

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize, except: [:new, :create]
+  #before_action :authorize, except: [:new, :create]
   
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
