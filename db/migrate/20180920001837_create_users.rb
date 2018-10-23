bcb5f976a6b0e69236e65d1ea1feca8c7608fdb7
Gesley Evening Silva Viana <gesley_123@hotmail.com>
2018-10-23 00:29:21 +0000

    Corrigindo tasks Paciente

--- db/migrate/20180920001837_create_users.rb
+++ db/migrate/20180920001837_create_users.rb
@@ -3,11 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :name
-      t.string :password
       t.integer :profile
 
       t.references :name, index: true
-      t.references :password, index: true
       t.references :email, index: true
       t.references :profile, index: true
 
