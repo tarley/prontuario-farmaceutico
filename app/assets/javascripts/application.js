3c56ecc8e1a54d8a1bec2b8d7ec717adac661676
Tarley <tarley.lana@gmail.com>
2018-11-26 23:13:41 +0000

    incluindo campos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -127,3 +127,152 @@ function mascaraData(val) {
     val.value = val.value.substring(0, 10);
   return true;
 }
+
+function createHeader(title) {
+  var a = $('<a class="collapse-link"></a>');
+  a.append('<i class="fa fa-chevron-up"></i>');
+  
+  var li = $('<li></li>');
+  li.append(a);
+  
+  var ul = $('<ul class="nav navbar-right panel_toolbox"></ul>');
+  ul.append(li);
+  
+  var div = $('<div class="x_title"></div>');
+  div.append(`<h2>${title}</h2>`);
+  div.append(ul);
+  div.append('<div class="clearfix"></div>');
+  
+  return div;
+}
+
+function createButton(texto, acao) {
+  var button = $('<button type="button" class="btn btn-primary"></button>');
+  button.append('<i class="fa fa-plus"></i>');
+  button.append(texto);
+  button.attr('onClick', acao);
+  
+  return button;
+}
+
+function createField(label, name, type='text') {
+  var label = $(`<label for="heard">${label}:</label>`);
+  
+  var fieldClass = 'form-control';
+  
+  switch (type) {
+    case 'textarea':
+      var field = $(`<textarea name="${name}"></textarea>`);
+      break;
+    case 'select':
+      var field = $(`<select name="${name}"></select>`);
+      break;
+    case 'data':
+      var divIcon = $('<div class="input-group-addon"></div>')
+                        .append('<span class="glyphicon glyphicon-th"></span>');
+      
+      var field = $('<div data-provide="datepicker"></div>')
+                      .append(`<input name="${name}" class="form-control" type="text"></input>`)
+                      .append(divIcon);
+                      
+      fieldClass = 'input-group date';
+      break;
+    default: 
+      var field = $(`<input name="${name}" type="${type}">`);
+  }
+  
+  field.attr('class', fieldClass);
+
+  var div = $('<div class="col-md-6 col-sm-12 col-xs-12 form-group"></div>');
+  div.append(label);
+  div.append(field);
+  
+  return div;
+}
+
+function createDiseasesHeader() {
+  var div = $('<div id="div-diseases-header" class="x_panel"></div>');
+  div.append(createHeader('Doenças'));
+  div.append('<div id="div-diseases-fields" class="x_content"></div>');
+  div.append(createButton('Adicionar Doença', 'createDiseasesFields()'));
+  
+  return div;
+}
+
+function createDiseasesFields() {
+  var id = $(":input[name='diseases[][id]']").length;
+  var field = $('<input type="hidden" name="diseases[][id]"/>');
+  field.val(id);
+  
+  var div = $('<div class="row x_content"></div>');
+  div.append(field);
+  div.append(createField('Nome da Doença:', 'diseases[][name]'));
+  div.append(createField('Descrição da doença:', 'diseases[][description]', 'textarea'));
+  $('#div-diseases-fields').append(div);
+  $('#div-diseases-fields').append(createPharmacotherapiesHeader(id));
+}
+
+function createPharmacotherapiesHeader(diseasesId) {
+  var div = $('<div class="x_panel"></div>');
+  div.append(createHeader('Farmacoterapia'));
+  div.append(`<div id="div-pharmacotherapies-fields_${diseasesId}" class="x_content"></div>`);
+  div.append(createButton('Adicionar Farmacoterapia', `createPharmacotherapiesFields(${diseasesId})`));
+  
+  return div;
+}
+
+function createPharmacotherapiesFields(diseasesId) {
+  var fieldDiseasesId = $('<input type="hidden" name="pharmacotherapies[][diseasesId]" />');
+  fieldDiseasesId.val(diseasesId);
+  
+  var id = $(":input[name='diseases[][id]']").length;
+  var fieldId = $('<input type="hidden" name="diseases[][id]"/>');
+  fieldId.val(id);
+  
+  var div = $('<div class="row x_content"></div>');
+  div.append(fieldId);
+  div.append(fieldDiseasesId);
+  div.append(createField('Remedio:', 'pharmacotherapies[][remedio]'));
+  div.append(createField('Dosagem:', 'pharmacotherapies[][dosagem]'));
+  div.append(createField('Segurança:', 'pharmacotherapies[][seguranca]'));
+  div.append(createField('Eficácia:', 'pharmacotherapies[][eficacia]'));
+  div.append(createField('Tempo de uso:', 'pharmacotherapies[][tempoUso]'));
+  div.append(createField('Dificuldade no uso:', 'pharmacotherapies[][dificuldadeUso]'));
+  div.append(createField('Selecione o PRM:', 'pharmacotherapies[][prm]', 'select'));
+  div.append(createField('Causa do PRM:', 'pharmacotherapies[][causaPRM]', 'select'));
+
+  var divFields = '#div-pharmacotherapies-fields_' + diseasesId;
+  $(divFields).append(div);
+  $(divFields).append(createCarePlansHeader(id));
+}
+
+function createCarePlansHeader(pharmacotherapiesId) {
+  var div = $('<div class="x_panel"></div>');
+  div.append(createHeader('Plano de Cuidados'));
+  div.append(`<div id="div-careplans-fields_${pharmacotherapiesId}" class="x_content"></div>`);
+  div.append(createButton('Adicionar Plano de Cuidados', `createCarePlansFields(${pharmacotherapiesId})`));
+  
+  return div;
+}
+
+function createCarePlansFields(pharmacotherapiesId) {
+  var fieldPharmacotherapiesId = $('<input type="hidden" name="carePlans[][pharmacotherapiesId]" />');
+  fieldPharmacotherapiesId.val(pharmacotherapiesId);
+  
+  var id = $(":input[name='carePlans[][id]']").length;
+  var fieldId = $('<input type="hidden" name="carePlans[][id]"/>');
+  fieldId.val(id);
+  
+  var div = $('<div class="row x_content"></div>');
+  div.append(fieldId);
+  div.append(fieldPharmacotherapiesId);
+  div.append(createField('Objetivo terapêutico:', 'carePlans[][objetivo]'));
+  div.append(createField('Conduta:', 'carePlans[][conduta]'));
+  div.append(createField('Data do Resultado:', 'carePlans[][data]', 'data'));
+  div.append(createField('Outra conduta:', 'carePlans[][outra]'));
+  div.append(createField('SFC:', 'carePlans[][sfc]', 'select'));
+
+  var divFields = '#div-careplans-fields_' + pharmacotherapiesId;
+  $(divFields).append(div);
+}
+                           
